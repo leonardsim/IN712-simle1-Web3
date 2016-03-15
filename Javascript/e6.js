63814654a3bf022f2b2b98d2f94c0ff411a907f5
leonardsim <SIMLE1@student.op.ac.nz>
2016-03-16 11:56:45 +1300

    Question 6 complete

---
+++ Javascript/e6.js
@@ -0,0 +1,16 @@
+function DayofBirthday(){
+  var date = new Date(1993,10,28);
+
+  var weekday = new Array(7);
+  weekday[0]=  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  console.log(weekday[date.getDay()]);
+}
+
+DayofBirthday();
